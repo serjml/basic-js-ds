6c3f88900431f8c77bf7d3b9efe12c06760dbf03
Serjml <Serjml@list.ru>
2022-04-30 14:30:08 +0300

    fead: solve queue

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,21 +13,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class Queue {
+  constructor() {
+    this.queue = new ListNode()
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.queue
+  }
+
+  enqueue(value) {
+    let newNode = new ListNode(value);
+    let head = this.queue;
+    if (!head.value) {
+      head.value = value;
+      return this.queue;
+    }
+    
+    let tail = this.queue;
+    while (tail.next) {
+      tail = tail.next;
+     }
+     tail.next = newNode;
+     return this.queue;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let el = this.queue.value;    
+    this.queue = this.queue.next;
+    return el;
   }
 }
 
